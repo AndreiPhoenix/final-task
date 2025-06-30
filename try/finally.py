8541ff2533023e26af3a58e5e916196415b69492
AndreiPhoenix <145689886+AndreiPhoenix@users.noreply.github.com>
2025-06-30 16:22:27 +0500

    Delete try directory

--- try/finally.py
+++
@@ -1,6 +0,0 @@
-def test_example(chrome_driver):
-    try:
-        chrome_driver.get("https://example.com")
-        assert "Example" in chrome_driver.title
-    finally:
-        chrome_driver.quit()  # Завершаем сессию, даже если тест упал
